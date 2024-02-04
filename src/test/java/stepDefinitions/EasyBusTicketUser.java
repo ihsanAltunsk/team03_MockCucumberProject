ddc1da662d7726ba8349da9fd5ecd1b926486ac8
AzimKaya <azimkaya777@gmail.com>
2024-02-04 20:09:11 +0300

    04/02/2024 Azim

--- src/test/java/stepDefinitions/EasyBusTicketUser.java
+++ src/test/java/stepDefinitions/EasyBusTicketUser.java
@@ -274,9 +274,7 @@ public class EasyBusTicketUser {
     public void clicks_on_the_privacy_policy_in_the_footer() {
         visitorHomePage.footerprivacypolicy.click();
 
-
     }
-      
     @Then("Visitor homapage header displays web elements")
     public void visitorHomapageHeaderDisplaysWebElements() {
         SoftAssert softAssert = new SoftAssert();
@@ -285,5 +283,145 @@ public class EasyBusTicketUser {
         for (int i = 0; i < 20 ; i++) {
             softAssert.assertTrue(visitorHomePage.listr.get(i).isDisplayed());
         }
+    }
+    @Then("Click on the {string} button on the header.")
+    public void click_on_the_button_on_the_headerPhoneNumber(String HeaderPhoneNumber ) {
+        visitorHomePage.HeaderPhoneNumber.click();
+    }
+    @Then("Click on the Twitter icon button on the header.")
+    public void click_on_the_twitter_icon_button_on_the_header() {
+        visitorHomePage.Headertiwetterİcon.click();
+    }
+    @Then("Verify that you are navigated to twitter page.")
+    public void verify_that_you_are_navigated_to_twitter_page() {
+        String expectedtwitterUrl="https://twitter.com/";
+        String actualtwitterUrl=Driver.getDriver().getCurrentUrl();
+        softAssert.assertEquals(actualUrl,expectedUrl,"twitter page is not visible");
+    }
+    @Then("Navigate to back.")
+    public void navigate_to_back() {
+        Driver.getDriver().navigate().back();
+    }
+    @Then("Click on the Facebook icon button on the header.")
+    public void click_on_the_facebook_icon_button_on_the_header() {
+        visitorHomePage.HeaderFacebook.click();
+    }
+    @Then("Verify that you are navigated to facebook page.")
+    public void verify_that_you_are_navigated_to_facebook_page() {
+        String expectedfacebookUrl="https://www.facebook.com";
+        String actualfacebookUrl=Driver.getDriver().getCurrentUrl();
+        softAssert.assertEquals(actualUrl,expectedUrl,"facebook page is not visible");
+    }
+    @Then("Click on the YouTube icon button on the header.")
+    public void click_on_the_you_tube_icon_button_on_the_header() {
+        visitorHomePage.HeaderYoutube.click();
+    }
+    @Then("Verify that you are navigated to youtube page.")
+    public void verify_that_you_are_navigated_to_youtube_page() {
+        actualUrl = Driver.getDriver().getCurrentUrl();
+        softAssert.assertTrue(actualUrl.contains("youtube.com"), "You are NOT navigated to youtube page!");
+    }
+    @Then("Click on the Instagram icon button on the header.")
+    public void click_on_the_ınstagram_icon_button_on_the_header() {
+        visitorHomePage.Headerİnstagramİcon.click();
+    }
+    @Then("Verify that you are navigated to instagram page.")
+    public void verify_that_you_are_navigated_to_instagram_page() {
+        String expectedinstagramUrl="https://www.instagram.com";
+        String actualinstagramUrl=Driver.getDriver().getCurrentUrl();
+        softAssert.assertEquals(actualUrl,expectedUrl,"instagram page is not visible");
+    }
+    @Then("Click on the Sign In button on the header.")
+    public void clickOnTheSignInButtonOnTheHeader() {
+        visitorHomePage.HeaderSignİgn.click();
+    }
+    @And("Verify that you are navigated to the Sign In page.")
+    public void verifyThatYouAreNavigatedToTheSignInPage() {
+        String expectedloginUrl="https://qa.easybusticket.com/login";
+        String actualloginUrl=Driver.getDriver().getCurrentUrl();
+        softAssert.assertEquals(actualloginUrl,expectedloginUrl,"login page is not visible");
+    }
+    @And("Click on the Sign Up button on the header.")
+    public void clickOnTheSignUpButtonOnTheHeader() {
+        visitorHomePage.HeaderSignUp.click();
+    }
+    @Then("Verify that you are navigated to the Sign Up page.")
+    public void verifyThatYouAreNavigatedToTheSignUpPage() {
+        String expectedUrl="https://qa.easybusticket.com/register";
+        String actualUrl=Driver.getDriver().getCurrentUrl();
+        softAssert.assertEquals(actualUrl,expectedUrl,"Sign In page is not visible");
+    }
+    @Then("Click on the Easybusticket ikon  on the header.")
+    public void click_on_the_easybusticket_ikon_on_the_header() {
+        visitorHomePage.HeaderLogo.click();
+    }
+    @Then("Verify that the page refreshes when the Easybusticket Logo ikon is clicked")
+    public void verify_that_the_page_refreshes_when_the_easybusticket_logo_ikon_is_clicked() {
+        String expectedLogoUrl="https://qa.easybusticket.com/";
+        String actualLogoUrl=Driver.getDriver().getCurrentUrl();
+        softAssert.assertEquals(actualLogoUrl,expectedLogoUrl,"Logo home page is not visible");
+    }
+    @Then("Click on the About button on the header.")
+    public void click_on_the_about_button_on_the_header() {
+        visitorHomePage.HeaderAbout.click();
+    }
+    @Then("Verify that you are navigated to the About page.")
+    public void verify_that_you_are_navigated_to_the_about_page() {
+        String expectedAboutUrl="https://qa.easybusticket.com/about-us";
+        String actualAboutUrl=Driver.getDriver().getCurrentUrl();
+        softAssert.assertEquals(actualAboutUrl,expectedAboutUrl,"About page is not visible");
+    }
+    @Then("Click on the FAQs button on the header.")
+    public void click_on_the_fa_qs_button_on_the_header() {
+        visitorHomePage.HeaderFaQs.click();
+    }
+    @Then("Verify that you are navigated to the FAQs page.")
+    public void verify_that_you_are_navigated_to_the_fa_qs_page() {
+        String expectedFAQsUrl="https://qa.easybusticket.com/faq";
+        String actualFAQsUrl=Driver.getDriver().getCurrentUrl();
+        softAssert.assertEquals(actualFAQsUrl,expectedFAQsUrl,"FAQs page is not visible");
+    }
+    @Then("Click on the Blog button on the header.")
+    public void click_on_the_blog_button_on_the_header() {
+        visitorHomePage.HeaderBlog.click();
+    }
+    @Then("Verify that you are navigated to the Blog page.")
+    public void verify_that_you_are_navigated_to_the_blog_page() {
+        String expectedBlogUrl="https://qa.easybusticket.com/blog";
+        String actualBlogUrl=Driver.getDriver().getCurrentUrl();
+        softAssert.assertEquals(actualBlogUrl,expectedBlogUrl,"Blog  page is not visible");
+    }
+    @Then("Click on the Contact button on the header.")
+    public void click_on_the_contact_button_on_the_header() {
+        visitorHomePage.HeaderContact.click();
+    }
+    @Then("Verify that you are navigated to the Contact page.")
+    public void verify_that_you_are_navigated_to_the_contact_page() {
+        String expectedContactUrl="https://qa.easybusticket.com/contact";
+        String actualContactUrl=Driver.getDriver().getCurrentUrl();
+        softAssert.assertEquals(actualContactUrl,expectedContactUrl,"Contact page is not visible");
+    }
+    @Then("Click on the BUY TICKETS button on the header.")
+    public void click_on_the_buy_tıckets_button_on_the_header() {
+        visitorHomePage.HeaderByTicket.click();
+    }
+    @Then("Verify that you are navigated to the Tickets page.")
+    public void verify_that_you_are_navigated_to_the_tickets_page() {
+        String expectedTicketsUrl="https://qa.easybusticket.com/tickets";
+        String actualTicketsUrl=Driver.getDriver().getCurrentUrl();
+        softAssert.assertEquals(actualTicketsUrl,expectedTicketsUrl,"Tickets page is not visible");
+    }
+    @And("Click on the {string}on the header.")
+    public void clickOnTheOnTheHeader(String arg0) {
+        visitorHomePage.HeaderLogo.click();
+    }
+    @And("Click on the {string} button  on the header.")
+    public void clickOnTheButtonOnTheHeader(String arg0) {
+        visitorHomePage.HeaderDasbordLinkButton.click();
+    }
+    @And("Verify that menu dasbord button is visible successfully")
+    public void verifyThatMenuDasbordButtonIsVisibleSuccessfully() {
+        SoftAssert softAssert=new SoftAssert();
+        softAssert.assertTrue(userSignIn.HeaderDashboard.isDisplayed(),"dashboard button is not visible");
     }
 }
--- src/test/resources/features/azim/US02.feature
+++ src/test/resources/features/azim/US02.feature
@@ -5,8 +5,67 @@ Feature: US02 Visitor logs in to the main page
     Given Visitor goes to the easybusticket homepage.
     Then Verify that home page is visible successfully
     And Verify that header is visible successfully
+    Then  User closes the page.
 
   Scenario: TC02 Visitor logs in to the main page
     Given Visitor goes to the easybusticket homepage.
     Then  Visitor homapage header displays web elements
+    Then  User closes the page.
+
+  Scenario: TC03 Visitor logs in to the main page
+    Given Visitor goes to the easybusticket homepage.
+    Then Verify that home page is visible successfully
+    And  Click on the "+1 (603) 624-1800" button on the header.
+    And  Click on the "info@easybusticket.com" button on the header.
+    And  Click on the Twitter icon button on the header.
+    Then Verify that you are navigated to twitter page.
+    But  Navigate to back.
+    And  Click on the Facebook icon button on the header.
+    Then Verify that you are navigated to facebook page.
+    But  Navigate to back.
+    Then Click on the YouTube icon button on the header.
+    Then Verify that you are navigated to youtube page.
+    But  Navigate to back.
+    And  Click on the Instagram icon button on the header.
+    Then Verify that you are navigated to instagram page.
+    Then  User closes the page.
+
+  Scenario: TC04 Visitor logs in to the main page
+    Given Visitor goes to the easybusticket homepage.
+    Then  Click on the Sign In button on the header.
+    And   Verify that you are navigated to the Sign In page.
+    But   Navigate to back.
+    And   Click on the Sign Up button on the header.
+    Then  Verify that you are navigated to the Sign Up page.
+    Then  User closes the page.
+
+  Scenario: TC05 Visitor logs in to the main page
+    Given Visitor goes to the easybusticket homepage.
+    Then  Click on the Easybusticket ikon  on the header.
+    Then  Verify that the page refreshes when the Easybusticket Logo ikon is clicked
+    Then  Click on the About button on the header.
+    Then  Verify that you are navigated to the About page.
+    But   Navigate to back.
+    Then  Click on the FAQs button on the header.
+    Then  Verify that you are navigated to the FAQs page.
+    But   Navigate to back.
+    Then  Click on the Blog button on the header.
+    Then  Verify that you are navigated to the Blog page.
+    But   Navigate to back.
+    Then  Click on the Contact button on the header.
+    Then  Verify that you are navigated to the Contact page.
+    But   Navigate to back.
+    Then  Click on the BUY TICKETS button on the header.
+    Then  Verify that you are navigated to the Tickets page.
+    Then  User closes the page.
+
+
+
+
+
+
+
+
+
+
 
